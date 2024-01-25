7dc8fd6d3db7227287ced64de0367cf5c0239903
Clayton Fasenmyer <bvswsaxplayer@gmail.com>
2024-01-25 17:03:56 -0600

    starting level system

---
+++ Player.gd
@@ -0,0 +1,18 @@
+extends Node
+
+@export var lvl = 0
+@export var xp = 0
+
+var previous_required_xp = 0
+var required_xp = 100
+
+func add
+	
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
