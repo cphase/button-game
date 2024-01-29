9be253881552979226ff516ae26a7dbdaca43687
Clayton Fasenmyer <claytonf@ksu.edu>
2024-01-29 11:24:13 -0600

    fixed framerate animation for level display

--- player_level_display.gd
+++ player_level_display.gd
@@ -2,19 +2,25 @@ extends Node2D
 
 var level_queue = []
 var animate = false
+var framerate = 12
+var physics_frame_count = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+func _physics_process(delta):
+	physics_frame_count += 1
+	if animate and physics_frame_count >= (1 / delta) / framerate:
+		$LevelDisplay.text = str("Level: ", level_queue.pop_front())
+		if (level_queue.size() == 0):
+			animate = false
+		physics_frame_count = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if animate:
-		$LevelDisplay.text = str("Level: ", level_queue.pop_front())
-		if (level_queue.size() == 0):
-			animate = false
-	
+	pass
 	
 func add_xp(old, new):
 	pass
